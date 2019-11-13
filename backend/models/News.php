cc1fecb4d014f3d7a259860376fab442fe35b411
prawee wongsa <prawee@hotmail.com>
2019-11-13 13:50:37 +0700

    override attributesLabels

--- backend/models/News.php
+++ backend/models/News.php
@@ -1,4 +1,18 @@
 <?php
 namespace backend\models;
 
-class News extends \common\models\News {}
+class News extends \common\models\News {
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'หัวข้อ',
+            'date_time' => 'Date Time',
+            'place' => 'Place',
+            'detail' => 'Detail',
+            'category_id' => 'Category ID',
+            'photo' => 'Photo',
+            'video' => 'Video',
+        ];
+    }
+}
